38ead87403d99b0209c4ad208dceb2c655e6ce33
이희원 <richready2011@gmail.com>
2021-12-05 17:43:11 +0900

    모바일플랫폼 설치까지

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.coach.friend.app',
+  appName: 'coach-friend',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
